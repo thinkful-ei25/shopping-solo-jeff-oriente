d1d76041a2d55b7466cbc1d5f1796ca3d6b7134f
Thomas Chun <tchun2055@gmail.com>
2018-09-28 09:36:59 -0700

    updated generateItemElement function

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const STORE = [
 ];
 
 function generateItemElement(item, itemIndex, template){
-  return `<li>${item.name}</li>`;
+  return `
+  <li class="js-item-index-element" 
+  data-item-index="${itemIndex}">
+    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
 }
 
 function generateShoppingItemsString(shoppingList) {
