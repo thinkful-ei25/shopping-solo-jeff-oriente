17d4b8e1a3dfc34de8ab570ca3ad3824c279cf6b
Thomas Chun <tchun2055@gmail.com>
2018-09-28 08:30:47 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function renderShoppingList() {
   // this function will be responsible for rendering the shopping list in
   // the DOM
@@ -21,7 +23,7 @@ function handleItemCheckClicked() {
 function handleDeleteItemClicked() {
   // this function will be responsible for when users want to delete a shopping list
   // item
-  console.log('`handleDeleteItemClicked` ran')
+  console.log('`handleDeleteItemClicked` ran');
 }
 
 // this function will be our callback when the page loads. it's responsible for
@@ -29,5 +31,10 @@ function handleDeleteItemClicked() {
 // that handle new item submission and user clicks on the "check" and "delete" buttons
 // for individual shopping list items.
 function handleShoppingList() {
-
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
 }
+
+$(handleShoppingList);
